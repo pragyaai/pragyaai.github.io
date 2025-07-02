26cc6f2f0ffee0bb54cdd22e36d3d63d54f072cf
jyotipatel00 <136438720+jyotipatel00@users.noreply.github.com>
2025-07-02 19:25:13 +0000

    Deploying to gh-pages from @ pragyaai/pragyaai.github.io@de0f65f77a44b2c1fe8e66776601a434d452a98b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-news",
+          title: "News",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-people",
           title: "People",
           description: "members of the lab",
